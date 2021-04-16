8eb7150b6ecbfc5660d1c4dd9428b7b32fa7c247
jwoojun <jjwjun10@gmail.com>
2021-04-16 23:37:57 +0900

    parallel

---
+++ Parallel/src/main/java/ExecutonExample.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.Executor;
+
+public class ExecutonExample implements Executor {
+    @Override
+    public void execute(Runnable task) {
+        task.run();
+    }
+
+    public static void main(String args []){
+        Executor executor = new ExecutonExample();
+        executor.execute(()-> System.out.println("Executor"));
+
+    }
+}
