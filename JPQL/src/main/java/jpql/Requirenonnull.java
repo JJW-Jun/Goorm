2f2ed37e8f80aa054e880ec16bc78cc1450fab21
jwoojun <jjwjun10@gmail.com>
2021-04-29 05:55:15 +0900

    batch

--- JPQL/src/main/java/jpql/Requirenonnull.java
+++ JPQL/src/main/java/jpql/Requirenonnull.java
@@ -2,13 +2,58 @@ package jpql;
 
 import lombok.NonNull;
 
+import java.util.List;
+import java.util.Objects;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.springframework.util.Assert.notNull;
+
 public class Requirenonnull {
     public static void main(String args[]) {
         notNull(null);
-
     }
 
-    static void notNull(@NonNull String str) {
-        System.out.println(str);
+//    static void notNull(@NonNull String str) {
+//        System.out.println("====================");
+//        assertNotNull(str);
+//        System.out.println("====================");
+//        System.out.println(str);
+//    }
+}
+class Theater {
+
+    private final List<String> movies;
+    private final SearchService searchService;
+
+    public Theater(@NonNull List<String> movies) {
+        System.out.println("0");
+        assertNotNull(movies, "message");
+        this.movies = movies;
+        this.searchService = new SearchService(movies);
+    }
+
+    public boolean findMovie(String userData) {
+        return searchService.findMovie(userData);
+    }
+
+    public static void main(String args[]) {
+
+        Theater theater = new Theater(null);
+        System.out.println("1");
+        boolean findMovie = theater.findMovie("아저씨");
+    }
+}
+
+
+class SearchService {
+
+    List<String> movies;
+
+    public SearchService(List<String> movies) {
+        this.movies = movies;
+    }
+
+    public boolean findMovie(String title) {
+        return movies.get(0).contains(title);
     }
 }
