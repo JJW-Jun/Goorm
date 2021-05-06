7c528c8e28529c293ac50b6587fb10754ee4c54c
jwoojun <jjwjun10@gmail.com>
2021-05-06 23:56:17 +0900

    testcode

---
+++ DesignPattern/src/main/java/builderpattern/second/ComputerDto.java
@@ -0,0 +1,36 @@
+package builderpattern.second;
+
+import java.util.Objects;
+
+public class Computer {
+    private String cpu;
+    private String ram;
+    private String storage;
+
+    public Computer(String cpu, String ram, String storage) {
+        this.cpu = Objects.requireNonNull(cpu, "Cpu is null");
+        this.ram = Objects.requireNonNull(ram, "Ram is null");
+        this.storage = Objects.requireNonNull(cpu, "Storage is null");
+    }
+
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "cpu='" + cpu + '\'' +
+                ", ram='" + ram + '\'' +
+                ", storage='" + storage + '\'' +
+                '}';
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public void setStorage(String storage) {
+        this.storage = storage;
+    }
+}
--- JPA/src/main/java/JpaMain.java
+++ JPA/src/main/java/JpaMain.java
@@ -1,31 +1,29 @@
 import javax.persistence.*;
 
 public class JpaMain {
-
     public static void main(String args[]) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            
-            Address address = new Address("Busan", "streetB", "051");
             Member memberA = new Member();
             memberA.setUsername("memberA");
-            memberA.setOfficeAddress(address);
-            memberA.setPeriod(new Period());
             em.persist(memberA);
 
-            Address newAddress = new Address("Seoul", "streetB", "051");
             Member memberB = new Member();
             memberB.setUsername("memberB");
-            memberB.setOfficeAddress(newAddress);
-            memberB.setPeriod(new Period());
             em.persist(memberB);
+            em.flush();
+            em.clear();
 
-
-
+            System.out.println("====================");
+            Member refMember = em.getReference(Member.class, memberB.getId());
+            em.detach(refMember);
+            refMember.getUsername();
+            System.out.println("====================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
